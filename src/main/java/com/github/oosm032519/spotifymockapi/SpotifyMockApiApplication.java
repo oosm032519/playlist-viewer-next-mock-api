41bd72cb4cf6d47fdf58633d54f3467ff80d3a5a
oosm032519 <ryooshima0519@gmail.com>
2025-01-28 18:18:38 +0900

    初回コミット

---
+++ src/main/java/com/github/oosm032519/spotifymockapi/SpotifyMockApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.oosm032519.spotifymockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyMockApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/oosm032519/spotifymockapi/SpotifyMockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oosm032519.spotifymockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
